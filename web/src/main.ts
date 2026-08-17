863dd28f99e068476f1e8df1f54b3b2f8959eec2
maxisoft <maxisoft.eu@gmail.com>
2026-08-17 20:47:58 +0200

    fix(types): cast Object.hasOwn polyfill to any for strict tsc verification

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,6 @@
 // Polyfills for legacy WebViews (Chromium < 93)
-if (typeof Object.hasOwn === 'undefined') {
-  Object.hasOwn = (obj: any, prop: PropertyKey) => Object.prototype.hasOwnProperty.call(obj, prop);
+if (typeof (Object as any).hasOwn === 'undefined') {
+  (Object as any).hasOwn = (obj: any, prop: PropertyKey) => Object.prototype.hasOwnProperty.call(obj, prop);
 }
 
 import { bootstrapApplication } from '@angular/platform-browser';
